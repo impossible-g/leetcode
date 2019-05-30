db2b7283a0afc3e5410120da21d7bf268ba1ce77
gq <1044588169@qq.com>
2019-05-30 18:07:53 +0800

    修改子数组的最小值之和

--- stack/907_sum_of_subbarray_minimums.py
+++ stack/907_sum_of_subbarray_minimums.py
@@ -1,19 +1,26 @@
 # _*_coding:utf-8_*_
 # __author: {a123456}
 class Solution:
+    """
+    归纳法可以知道每个元素 A[i] 被加入到最终结果的次数为其左邻居中大于它、右邻居中不小于它的元素长度 left 、 right （包括自身）的乘积。
+    """
+
     def sumSubarrayMins(self, A: [int]) -> int:
         stack = []
-        sum = 0
+        ret = 0
+
         A.insert(0, 0)
         A.append(0)
         for i in range(len(A)):
             while stack and A[i] < A[stack[-1]]:
-                out = stack.pop(-1)
-                # (out - stack[-1]) 统计当前数左边为最小的次数
-                # (i - out) 统计当前数右边为最小的次数
-                sum += (out - stack[-1]) * (i - out) * A[out]
+                out = stack.pop()
+                left = out - stack[-1]
+                right = i - out
+                ret += A[out] * left * right
+
             stack.append(i)
-        return sum % (pow(10, 9) + 7)
+
+        return ret % ((10 ** 9) + 7)
 
 
 if __name__ == '__main__':
