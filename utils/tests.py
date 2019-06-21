a9f8a8c76c87d325e00897903269d8633403ab24
gq <1044588169@qq.com>
2019-06-21 19:01:25 +0800

    堆数据结构

--- utils/tests.py
+++ utils/tests.py
@@ -1,5 +1,66 @@
-# _*_coding:utf-8_*_
-# __author: a123456
+class Heap:
+    def __init__(self, li):
+        self._values = li
+        if li:
+            self.build_heap()
 
-for i in range(5 - 2, -1, -1):
-    print(i)
+    def is_empty(self):
+        return not self._values
+
+    # 取堆顶元素
+    def peek(self):
+        if self.is_empty():
+            raise ValueError("堆为空")
+        return self._values[0]
+
+    # 上浮
+    def siftup(self, value):
+        last_index = len(self._values) - 1
+        values, i, j = self._values, last_index, (last_index - 1) // 2
+
+        while i > 0 and value < values[j]:
+            values[i] = values[j]
+            i, j = j, (j - 1) // 2
+
+        values[i] = value
+
+    # 插入
+    def push(self, value):
+        self._values.append(None)
+        self.siftup(value)
+
+    # 下沉
+    def siftdown(self, value, index):
+        values, index, i = self._values, index, index * 2 + 1
+        max_index = len(values)
+
+        while i < max_index:
+            if i + 1 < max_index and values[i + 1] < values[i]:
+                i += 1
+            if value < values[i]:
+                break
+            values[index] = values[i]
+            index = i
+            i = 2 * i + 1
+        values[index] = value
+
+    # 弹出
+    def pop(self):
+        if self.is_empty():
+            raise ValueError("堆为空")
+        values = self._values
+        e0 = values[0]
+        e = values.pop()
+        if len(values) > 0:
+            self.siftdown(e, 0)
+        return e0
+
+    # 从数组构建堆
+    def build_heap(self):
+        for index in range(len(self._values) // 2 - 1, -1, -1):
+            self.siftdown(self._values[index], index)
+
+
+if __name__ == '__main__':
+    a = Heap([10, 9, 6, 7, 8, 2, 5, 1, 4, 3])
+    print(a)
--- utils/tools.py
+++ utils/tools.py
@@ -266,5 +266,66 @@ class TrieNode:
         return t
 
 
+class Heap:
+    def __init__(self):
+        self.data_list = []
+
+    def get_parent_index(self, index):
+        if index == 0 or index > len(self.data_list) - 1:
+            return None
+        return (index - 1) >> 1
+
+    def swap(self, index1, index2):
+        self.data_list[index1], self.data_list[index2] = self.data_list[index2], self.data_list[index1]
+
+    def insert(self, value):
+        self.data_list.append(value)
+        index = len(self.data_list) - 1
+        parent = self.get_parent_index(index)
+
+        while parent is not None and self.data_list[parent] < self.data_list[index]:
+            self.swap(parent, index)
+            index = parent
+            parent = self.get_parent_index(parent)
+
+    def rm_max(self):
+        rm_value = self.data_list[0]
+        self.data_list[0] = self.data_list[1]
+        del self.data_list[-1]
+
+        self.heapify(0)
+        return rm_value
+
+    def heapify(self, index):
+        total_index = len(self.data_list) - 1
+
+        while 1:
+            max_value_index = index
+
+            n1 = 2 * index + 1
+            if n1 <= total_index and self.data_list[n1] > self.data_list[max_value_index]:
+                max_value_index = n1
+
+            n2 = 2 * index + 2
+            if n2 <= total_index and self.data_list[n2] > self.data_list[max_value_index]:
+                max_value_index = n2
+
+            if max_value_index == index:
+                break
+
+            self.swap(index, max_value_index)
+            index = max_value_index
+
+    @classmethod
+    def test(cls):
+        heap = cls()
+        a = [10, 9, 6, 7, 8, 2, 5, 1, 4, 3]
+        for i in a:
+            heap.insert(i)
+        heap.rm_max()
+        print(heap.data_list)
+
+
 if __name__ == '__main__':
     TrieNode.test()
+    Heap.test()
